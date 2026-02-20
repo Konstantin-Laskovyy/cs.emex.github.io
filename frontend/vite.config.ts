cde24fa2355883983821befcc59bb1411ef2c496
Konstantin Laskovyy <Konstantin.Laskovyy@emex.kz>
2026-02-20 11:15:51 +0500

    Fix Vite base for GitHub Pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/cs.emex.github.io/",
   plugins: [react()],
-  server: {
-    port: 5173,
-  },
 });
-
